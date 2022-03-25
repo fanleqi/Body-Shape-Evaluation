14dbaf7a43a0e6f644aac74b8765f5bc8f8b031d
xiaoqi <leqifan@foxmail.com>
2022-03-25 22:30:31 +0800

    feat:BMI及体脂率等级

--- src/views/result.vue
+++ src/views/result.vue
@@ -26,7 +26,284 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {}
+  },
+  created() {
+    this.getShapeList()
+  },
+  methods: {
+    getShapeList() {
+      let shapeList = JSON.parse(localStorage.getItem('userInfo'))
+      let BMI = (shapeList.weight / Math.pow(shapeList.height, 2)).toFixed(1)
+      shapeList.bodyFatRate = shapeList.bodyFatRate.toFixed()
+      let BMILevel = this.getBMILevel(BMI)
+      let bodyFatRateLevel = this.getBodyFatRateLevel(
+        shapeList.gender,
+        shapeList.age,
+        shapeList.bodyFatRate
+      )
+      console.log(BMILevel, bodyFatRateLevel)
+    },
+    getBMILevel(bmi) {
+      if (bmi < 18.5) {
+        return '偏轻'
+      } else if (bmi < 24) {
+        return '标准'
+      } else if (bmi < 28) {
+        return '超重'
+      } else {
+        return '过重'
+      }
+    },
+    getBodyFatRateLevel(gender, age, bodyFatRate) {
+      let ageLevel = 0
+      if (age < 18) {
+        ageLevel = 1
+      } else if (age >= 18 && age <= 25) {
+        ageLevel = 2
+      } else if (age >= 26 && age <= 35) {
+        ageLevel = 3
+      } else if (age >= 36 && age <= 45) {
+        ageLevel = 4
+      } else if (age >= 46 && age <= 55) {
+        ageLevel = 5
+      } else if (age >= 56 && age <= 65) {
+        ageLevel = 6
+      } else {
+        ageLevel = 7
+      }
+      if (gender === 0) {
+        if (ageLevel === 1) {
+          if (bodyFatRate <= 5) {
+            return '非常低'
+          } else if (bodyFatRate <= 10) {
+            return '低'
+          } else if (bodyFatRate <= 25) {
+            return '平均'
+          } else if (bodyFatRate <= 31) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 2) {
+          if (bodyFatRate <= 7) {
+            return '非常低'
+          } else if (bodyFatRate <= 10) {
+            return '低'
+          } else if (bodyFatRate <= 13) {
+            return '偏低'
+          } else if (bodyFatRate <= 17) {
+            return '平均'
+          } else if (bodyFatRate <= 21) {
+            return '偏高'
+          } else if (bodyFatRate <= 27) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 3) {
+          if (bodyFatRate <= 12) {
+            return '非常低'
+          } else if (bodyFatRate <= 15) {
+            return '低'
+          } else if (bodyFatRate <= 18) {
+            return '偏低'
+          } else if (bodyFatRate <= 21) {
+            return '平均'
+          } else if (bodyFatRate <= 24) {
+            return '偏高'
+          } else if (bodyFatRate <= 29) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 4) {
+          if (bodyFatRate <= 15) {
+            return '非常低'
+          } else if (bodyFatRate <= 18) {
+            return '低'
+          } else if (bodyFatRate <= 21) {
+            return '偏低'
+          } else if (bodyFatRate <= 24) {
+            return '平均'
+          } else if (bodyFatRate <= 26) {
+            return '偏高'
+          } else if (bodyFatRate <= 29) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 5) {
+          if (bodyFatRate <= 17) {
+            return '非常低'
+          } else if (bodyFatRate <= 20) {
+            return '低'
+          } else if (bodyFatRate <= 23) {
+            return '偏低'
+          } else if (bodyFatRate <= 25) {
+            return '平均'
+          } else if (bodyFatRate <= 28) {
+            return '偏高'
+          } else if (bodyFatRate <= 31) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 6) {
+          if (bodyFatRate <= 18) {
+            return '非常低'
+          } else if (bodyFatRate <= 21) {
+            return '低'
+          } else if (bodyFatRate <= 24) {
+            return '偏低'
+          } else if (bodyFatRate <= 26) {
+            return '平均'
+          } else if (bodyFatRate <= 28) {
+            return '偏高'
+          } else if (bodyFatRate <= 31) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else {
+          if (bodyFatRate <= 18) {
+            return '非常低'
+          } else if (bodyFatRate <= 21) {
+            return '低'
+          } else if (bodyFatRate <= 23) {
+            return '偏低'
+          } else if (bodyFatRate <= 25) {
+            return '平均'
+          } else if (bodyFatRate <= 27) {
+            return '偏高'
+          } else if (bodyFatRate <= 30) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        }
+      }else{
+        if (ageLevel === 1) {
+          if (bodyFatRate <= 12) {
+            return '非常低'
+          } else if (bodyFatRate <= 15) {
+            return '低'
+          } else if (bodyFatRate <= 30) {
+            return '平均'
+          } else if (bodyFatRate <= 36) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 2) {
+          if (bodyFatRate <= 17) {
+            return '非常低'
+          } else if (bodyFatRate <= 20) {
+            return '低'
+          } else if (bodyFatRate <= 23) {
+            return '偏低'
+          } else if (bodyFatRate <= 25) {
+            return '平均'
+          } else if (bodyFatRate <= 28) {
+            return '偏高'
+          } else if (bodyFatRate <= 32) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 3) {
+          if (bodyFatRate <= 18) {
+            return '非常低'
+          } else if (bodyFatRate <= 21) {
+            return '低'
+          } else if (bodyFatRate <= 23) {
+            return '偏低'
+          } else if (bodyFatRate <= 26) {
+            return '平均'
+          } else if (bodyFatRate <= 30) {
+            return '偏高'
+          } else if (bodyFatRate <= 35) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 4) {
+          if (bodyFatRate <= 19) {
+            return '非常低'
+          } else if (bodyFatRate <= 23) {
+            return '低'
+          } else if (bodyFatRate <= 26) {
+            return '偏低'
+          } else if (bodyFatRate <= 29) {
+            return '平均'
+          } else if (bodyFatRate <= 32) {
+            return '偏高'
+          } else if (bodyFatRate <= 38) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 5) {
+          if (bodyFatRate <= 22) {
+            return '非常低'
+          } else if (bodyFatRate <= 25) {
+            return '低'
+          } else if (bodyFatRate <= 28) {
+            return '偏低'
+          } else if (bodyFatRate <= 31) {
+            return '平均'
+          } else if (bodyFatRate <= 35) {
+            return '偏高'
+          } else if (bodyFatRate <= 38) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else if (ageLevel === 6) {
+          if (bodyFatRate <= 23) {
+            return '非常低'
+          } else if (bodyFatRate <= 27) {
+            return '低'
+          } else if (bodyFatRate <= 30) {
+            return '偏低'
+          } else if (bodyFatRate <= 33) {
+            return '平均'
+          } else if (bodyFatRate <= 36) {
+            return '偏高'
+          } else if (bodyFatRate <= 38) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        } else {
+          if (bodyFatRate <= 21) {
+            return '非常低'
+          } else if (bodyFatRate <= 26) {
+            return '低'
+          } else if (bodyFatRate <= 29) {
+            return '偏低'
+          } else if (bodyFatRate <= 32) {
+            return '平均'
+          } else if (bodyFatRate <= 35) {
+            return '偏高'
+          } else if (bodyFatRate <= 38) {
+            return '高'
+          } else {
+            return '非常高'
+          }
+        }
+      }
+    },
+    // getBodyShape(BMILevel,bodyFatRateLevel){
+      
+    // },
+    // getWaistWarn(waistline){
+
+    // }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
